a3e8f1a68a04e19a32d75db03d1e40acfdfe7f40
Bruno Fernando <bfernandodco@protonmail.com>
2023-08-24 15:02:09 -0300

    Adicionado sons Pom e Clap onclick.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
-document.querySelector('.tecla_pom'); //o comando document permite acessar o HTML para implementações
-document.querySelector('input[type=tel]');
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play(); //A função .play() reproduz o áudio
+}
+
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom; //sem os () a função é armazenada no atributo onClick
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
