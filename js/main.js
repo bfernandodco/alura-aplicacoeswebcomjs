0fd2514268ec4f8881acc435bb9962ef3fc09376
Bruno Fernando <bfernandodco@protonmail.com>
2023-08-24 15:51:24 -0300

    Adicionado listaDeTeclas iterativa.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* 
 function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play(); //A função .play() reproduz o áudio
 }
@@ -8,3 +9,22 @@ function tocaSomClap() {
 
 document.querySelector('.tecla_pom').onclick = tocaSomPom; //sem os () a função é armazenada no atributo onClick
 document.querySelector('.tecla_clap').onclick = tocaSomClap;
+*/
+
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+//const = constante | let = variável
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+    /*função genérica para que a função tocaSom()
+    não seja executada imediatamente*/
+    listaDeTeclas[contador].onclick = function() {
+        tocaSom('#som_tecla_pom');
+    }
+    contador++;
+    console.log(contador);
+}
